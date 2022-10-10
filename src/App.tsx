7089dab163cfa732b02d078aafb06b25d52f51b6
Ankush Thakur <ankush.thakur53@gmail.com>
2022-10-10 10:24:58 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import './App.css';
+import { MuiBreadcrumbs } from './components/MuiBreadcrumbs';
+// import { MuiLink } from './components/MuiLink';
+// import { MuiImageList } from './components/MuiImageList';
+// import { MuiNavbar } from './components/MuiNavbar';
+// import { MuiAccordion } from './components/MuiAccordion';
+// import { MuiCard } from './components/MuiCard';
+// import { MuiAutoComplete } from './components/MuiAutoComplete';
+// import { MuiLayout } from './components/MuiLayout';
+// import { MuiTypography } from './components/MuiTypography';
+// import { MuiButton } from './components/MuiButton';
+// import { MuiTextField } from './components/MuiTextField';
+// import { MuiSelect } from './components/MuiSelect';
+// import { MuiRadioButton } from './components/MuiRadioButton';
+// import { MuiCheckbox } from './components/MuiCheckbox';
+// import { MuiSwitch } from './components/MuiSwitch';
+// import { MuiRating } from './components/MuiRating';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MuiTypography /> */}
+      {/* <MuiButton /> */}
+      {/* <MuiTextField /> */}
+      {/* <MuiSelect /> */}
+      {/* <MuiRadioButton /> */}
+      {/* <MuiCheckbox /> */}
+      {/* <MuiSwitch /> */}
+      {/* <MuiRating /> */}
+      {/* <MuiAutoComplete /> */}
+      {/* <MuiLayout /> */}
+      {/* <MuiCard /> */}
+      {/* <MuiAccordion /> */}
+      {/* <MuiImageList /> */}
+      {/* <MuiNavbar /> */}
+      {/* <MuiLink /> */}
+      <MuiBreadcrumbs />
+    </div>
+  );
+}
+
+export default App;
